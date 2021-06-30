1686173e3adde0e3c6ea7d00111e33fc15596e15
Jeffrey Liu <yjeffliu@gmail.com>
2021-06-29 19:50:23 -0700

    99 message responder

--- Chesster.py
+++ Chesster.py
@@ -1,14 +1,47 @@
 import os
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = [member.name for member in guild.members]
+    print(members)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
