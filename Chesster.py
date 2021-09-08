26f2fe7ed69101cf13d8673c9bdd462b4cf8854b
Jeffrey Liu <yjeffliu@gmail.com>
2021-09-08 00:44:39 -0700

    create chess board

--- Chesster.py
+++ Chesster.py
@@ -3,6 +3,7 @@ import random
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from PIL import Image
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -25,7 +26,15 @@ async def start_game(ctx, p1: discord.Member, p2: discord.Member):
     global turn
     global game_started
 
-    board = [0 for i in range(9)]
+    board = [[2, 3, 4, 5, 6, 4, 3, 2], 
+             [1, 1, 1, 1, 1, 1, 1, 1], 
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [7, 7, 7, 7, 7, 7, 7, 7],
+             [8, 9, 10, 11, 12, 10, 9, 8]]
+
     player1 = p1
     player2 = p2
 
@@ -89,22 +98,76 @@ async def place(ctx, row: int, col: int):
             await ctx.send(f"It is {player1}'s turn.")
 
 async def draw_board(ctx):
-    line = ""
-    count = 0
-    for val in board:
-        if(val == 0):
-            line += ":white_large_square:"
-            count += 1
-        elif(val == 1):
-            line += ":o2:"
-            count += 1
-        else:
-            line += ":regional_indicator_x:"
-            count += 1
-        if(count == num_cols):
-            await ctx.send(line)
-            line = ""
-            count = 0
+    cboard = Image.open("chessboard.png")
+    cboard = cboard.convert("RGBA")
+    
+    pieces = Image.open("chesspieces.png")
+    pieces = pieces.convert("RGBA")
+
+    width, height = pieces.size
+    pieces = pieces.resize((int(0.8 * width), int(0.8 * height)))
+
+    width, height = pieces.size
+    width /= 6
+    height /= 2
+
+    width = int(width)
+    height = int(height)
+
+    wking = pieces.crop((0, 0, width, height))
+    wqueen = pieces.crop((width, 0, width * 2, height))
+    wbishop = pieces.crop((width * 2, 0, width * 3, height))
+    wknight = pieces.crop((width * 3, 0, width * 4, height))
+    wrook = pieces.crop((width * 4, 0, width * 5, height))
+    wpawn = pieces.crop((width * 5, 0, width * 6, height))
+
+    bking = pieces.crop((0, height, width, height * 2))
+    bqueen = pieces.crop((width, height, width * 2, height * 2))
+    bbishop = pieces.crop((width * 2, height, width * 3, height * 2))
+    bknight = pieces.crop((width * 3, height, width * 4, height * 2))
+    brook = pieces.crop((width * 4, height, width * 5, height * 2))
+    bpawn = pieces.crop((width * 5, height, width * 6, height * 2))
+
+    displace = 32
+    square_size = 97
+    curr_x = 0
+    curr_y = 0
+
+    for row in board:
+        for piece in row:
+            if piece == 1:
+                cboard.paste(bpawn, (curr_x * square_size + displace, curr_y * square_size + displace), bpawn)
+            elif piece == 2:
+                cboard.paste(brook, (curr_x * square_size + displace, curr_y * square_size + displace), brook)
+            elif piece == 3:
+                cboard.paste(bknight, (curr_x * square_size + displace, curr_y * square_size + displace), bknight)
+            elif piece == 4:
+                cboard.paste(bbishop, (curr_x * square_size + displace, curr_y * square_size + displace), bbishop)
+            elif piece == 5:
+                cboard.paste(bqueen, (curr_x * square_size + displace, curr_y * square_size + displace), bqueen)
+            elif piece == 6:
+                cboard.paste(bking, (curr_x * square_size + displace, curr_y * square_size + displace), bking)
+            elif piece == 7:
+                cboard.paste(wpawn, (curr_x * square_size + displace, curr_y * square_size + displace), wpawn)
+            elif piece == 8:
+                cboard.paste(wrook, (curr_x * square_size + displace, curr_y * square_size + displace), wrook)
+            elif piece == 9:
+                cboard.paste(wknight, (curr_x * square_size + displace, curr_y * square_size + displace), wknight)
+            elif piece == 10:
+                cboard.paste(wbishop, (curr_x * square_size + displace, curr_y * square_size + displace), wbishop)
+            elif piece == 11:
+                cboard.paste(wqueen, (curr_x * square_size + displace, curr_y * square_size + displace), wqueen)
+            elif piece == 12:
+                cboard.paste(wking, (curr_x * square_size + displace, curr_y * square_size + displace), wking)
+
+            curr_x += 1
+        
+        curr_x = 0
+        curr_y += 1
+
+    cboard.save("currboard.png")
+   
+    await ctx.send(file=discord.File('currboard.png'))
         
 async def check_win_con(ctx):
     global game_started
